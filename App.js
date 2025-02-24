1af63d3bdc198a6c1a99cef4d51523760aa81fb3
katirantala <katirantala2@gmail.com>
2025-02-24 12:10:22 +0200

    Added navigation.

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './navigation/Navigator';
+import { StyleSheet, View } from 'react-native'; 
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ navigation/Navigator.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+import AddLocationScreen from '../screens/AddLocationScreen.js';
+import LocationsListScreen from '../screens/LocationsListScreen.js';
+import MapScreen from '../screens/MapScreen.js';
+
+const Tab = createBottomTabNavigator();
+
+const LIST = "LIST";
+const ADD = "ADD";
+const MAP = "MAP";
+
+const icons = {
+    [LIST]: "bars", 
+    [ADD]: "plus",  
+    [MAP]: "enviroment",    
+}
+
+const Navigator = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen 
+                name="Locations" 
+                component={LocationsListScreen} 
+                options={{
+                    tabBarIcon: () => <AntDesign name={icons[LIST]} size={24} />
+                }}
+            />
+            <Tab.Screen
+                name="Add location" 
+                component={AddLocationScreen}
+                options={{
+                    tabBarIcon: () => <AntDesign name={icons[ADD]} size={24} />
+                }}
+            />
+            <Tab.Screen 
+                name="Map" 
+                component={MapScreen} 
+                options={{
+                    tabBarIcon: () => <AntDesign name={icons[MAP]} size={24} />
+                }}
+            />
+        </Tab.Navigator>
+    );
+};
+
+export default Navigator;
