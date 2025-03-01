4847bacda793133057292fd410541ac4f4976d0b
katirantala <katirantala2@gmail.com>
2025-03-01 19:12:10 +0200

    Fixed typo.

--- screens/LocationsListScreen.js
+++ screens/LocationsListScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons';
 import { useLocations } from '../firebase/FirestoreController.js';
 import styles from '../styles/Styles.js';
 
@@ -7,13 +7,13 @@ const LocationsListScreen = ({ navigation }) => {
     const locations = useLocations();
 
     const renderItem = ({ item }) => (
-        <TouchableOpacity 
+        <TouchableOpacity
             style={styles.itemContainer}
             onPress={() => navigation.navigate('Map', { locationName: item.name })}
         >
             <View style={styles.itemRow}>
                 <Text style={styles.itemName}>{item.name}</Text>
-                <MaterialIcons style={styles.mapIcon} name="place" size={24}   /> 
+                <MaterialIcons style={styles.mapIcon} name="place" size={24} />
             </View>
             <Text style={styles.itemDescription}>{item.description}</Text>
             <Text style={styles.itemRating}>Rating: {item.rating}</Text>
@@ -22,11 +22,11 @@ const LocationsListScreen = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity 
-                style={styles.LocationListButton} 
-                onPress={() => navigation.navigate('AddLocation')} 
+            <TouchableOpacity
+                style={styles.LocationListButton}
+                onPress={() => navigation.navigate('AddLocation')}
             >
-                <Text style={styles.buttonText}>Add location</Text> 
+                <Text style={styles.buttonText}>Add location</Text>
             </TouchableOpacity>
             {locations.length === 0 ? (
                 <Text style={styles.emptyMessage}>No locations found. Add a new location. </Text>
